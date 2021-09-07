12ade4f1a9800741be30838fe55207196e64979f
bischtob <tobias.bischoff@pm.me>
2021-09-06 17:27:38 -0700

    docstrings and lintering

--- opterax/_src/eki.py
+++ opterax/_src/eki.py
@@ -1,3 +1,5 @@
+"""Approximate gradients through Ensemble Kalman Inversion."""
+
 import chex
 import jax
 import jax.numpy as jnp
@@ -5,15 +7,25 @@ import jax.numpy as jnp
 from typing import Callable
 
 def grad_eki(loss_fcn: Callable, obs_err_cov: chex.Array) -> Callable:
+  """Produces an approximate gradient function.
+  Args:
+    loss_fcn: a symmetric loss function that evaluates the distance
+    between truth and predictions.
+    obs_err_cov: the covariance matrix of observational noise.
+  Returns:
+    A function that approximately evaluates the gradient of a forward model,
+    given model parameters, forward model predictions, and truth to evaluate
+    against.
+  """
 
   # just like jax.grad, we use a closure here
-  def _grad_eki(params:chex.Array, y_pred:chex.Array, ys:chex.Array) -> chex.Array:
+  def _grad(params:chex.Array, y_pred:chex.Array, ys:chex.Array) -> chex.Array:
     n_par = params.shape[0]
     n_obs = y_pred.shape[0]
-  
+
     # equation (13) in stuart and kovachki
     grad_y = jax.vmap(jax.grad(loss_fcn), in_axes=(1, None))(y_pred, ys).T
-  
+
     # cross-covariances
     # TODO!: need to be able to work with JAX PyTrees
     cov_pe = jnp.atleast_2d(jnp.cov(params, y_pred, bias=True)[
@@ -26,6 +38,4 @@ def grad_eki(loss_fcn: Callable, obs_err_cov: chex.Array) -> Callable:
     grad = jnp.matmul(cov_pe, tmp)  # [n_par * n_ens]
     return grad
 
-  return _grad_eki
-
-
+  return _grad
